d58e48204a604ab7b9fc93bde131e3f80f8deb97
Pavion <priakhine@yahoo.de>
2017-12-30 10:35:40 +0100

    Moving threads to a separate package

--- src/main/java/org/openhab/binding/synologysurveillancestation/internal/thread/SynoApiThreadEvent.java
+++ src/main/java/org/openhab/binding/synologysurveillancestation/internal/thread/SynoApiThreadEvent.java
@@ -6,7 +6,7 @@
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  */
-package org.openhab.binding.synologysurveillancestation.internal;
+package org.openhab.binding.synologysurveillancestation.internal.thread;
 
 import java.time.ZonedDateTime;
 import java.util.HashMap;

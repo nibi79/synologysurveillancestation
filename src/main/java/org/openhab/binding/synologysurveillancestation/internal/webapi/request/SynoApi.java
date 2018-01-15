b5116c3c101b7aee9fca5dea70587a14912aebe8
Pavion <priakhine@yahoo.de>
2018-01-15 21:07:55 +0100

    Further increase connection timeouts.
    It seems, openHAB has no problem with waiting for a thread. It should
    not be a common issue but shouldn't affect fast connections either.

--- src/main/java/org/openhab/binding/synologysurveillancestation/internal/webapi/request/SynoApi.java
+++ src/main/java/org/openhab/binding/synologysurveillancestation/internal/webapi/request/SynoApi.java
@@ -47,7 +47,7 @@ public interface SynoApi {
     public static final String METHOD_MOVE = "Move";
     public static final String METHOD_SWITCH = "Switch";
 
-    public static final int API_CONNECTION_TIMEOUT = 4000;
+    public static final int API_CONNECTION_TIMEOUT = 8000;
 
     public SynoApiConfig getApiConfig();
 }

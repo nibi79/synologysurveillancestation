269c73eae90c90ff102f3cc70ab54602ab4c8b4f
Pavion <priakhine@yahoo.de>
2020-07-16 21:11:39 +0200

    Update SynoDynamicStateDescriptionProvider
    Trying to fix:
    
    bundle org.openhab.binding.synologysurveillancestation:2.5.6.202006130946 (257)[org.openhab.binding.synologysurveillancestation.internal.SynoHandlerFactory(257)] : The setDynamicStateDescriptionProvider method has thrown an exception
    java.lang.IllegalArgumentException: argument type mismatch

--- src/main/java/org/openhab/binding/synologysurveillancestation/internal/discovery/SynoDynamicStateDescriptionProvider.java
+++ src/main/java/org/openhab/binding/synologysurveillancestation/internal/discovery/SynoDynamicStateDescriptionProvider.java
@@ -17,13 +17,13 @@ import java.util.Locale;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
-import org.eclipse.jdt.annotation.NonNull;
 import org.eclipse.jdt.annotation.NonNullByDefault;
 import org.eclipse.jdt.annotation.Nullable;
 import org.eclipse.smarthome.core.thing.Channel;
 import org.eclipse.smarthome.core.thing.ChannelUID;
 import org.eclipse.smarthome.core.thing.type.DynamicStateDescriptionProvider;
 import org.eclipse.smarthome.core.types.StateDescription;
+import org.eclipse.smarthome.core.types.StateDescriptionFragmentBuilder;
 import org.eclipse.smarthome.core.types.StateOption;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Deactivate;
@@ -36,8 +36,7 @@ import org.osgi.service.component.annotations.Deactivate;
  * @author Pavion - Contribution
  */
 @NonNullByDefault
-@Component(service = { DynamicStateDescriptionProvider.class,
-        SynoDynamicStateDescriptionProvider.class }, immediate = true)
+@Component(service = { DynamicStateDescriptionProvider.class, SynoDynamicStateDescriptionProvider.class })
 public class SynoDynamicStateDescriptionProvider implements DynamicStateDescriptionProvider {
 
     private final Map<ChannelUID, List<StateOption>> channelOptionsMap = new ConcurrentHashMap<>();
@@ -47,16 +46,16 @@ public class SynoDynamicStateDescriptionProvider implements DynamicStateDescript
     }
 
     @Override
-    public @Nullable StateDescription getStateDescription(@NonNull Channel channel, @Nullable StateDescription original,
+    public @Nullable StateDescription getStateDescription(Channel channel, @Nullable StateDescription original,
             @Nullable Locale locale) {
         List<StateOption> options = channelOptionsMap.get(channel.getUID());
-
-        if (original != null) {
-            return new StateDescription(original.getMinimum(), original.getMaximum(), original.getStep(),
-                    original.getPattern(), original.isReadOnly(), options);
+        if (options == null) {
+            return null;
         }
 
-        return new StateDescription(null, null, null, null, false, options);
+        StateDescriptionFragmentBuilder builder = (original == null) ? StateDescriptionFragmentBuilder.create()
+                : StateDescriptionFragmentBuilder.create(original);
+        return builder.withOptions(options).build().toStateDescription();
     }
 
     @Deactivate
